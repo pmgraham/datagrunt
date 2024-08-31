b4986e6b65ff6ea1a39278523c8e81c6294b3e6d
Martin Graham <pmgraham@gmail.com>
2024-08-30 21:23:48 -0400

    Reorganize (#4)
    * add missing docstring
    
    * update readme code example
    
    * remove unused code
    
    * created queries class to house duckdb queries
    
    * created queries class to house duckdb queries
    
    * removed query from csvfile; add later in queries module
    
    * removed query from csvfile; add later in queries module
    
    * add missing docstrings
    
    * trim whitespaces
    
    * update ability to query csv data; add relevant class attributes to support queries
    
    * reference app db connection
    
    * prevent returning None value
    
    * explicitly name export queries as such
    
    * add missing docstring
    
    * trim whitespace
    
    * proper spacing between functions
    
    * remove unneeded method
    
    * comment update
    
    * update docstring
    
    * add sample csv method and reorder methods in the class
    
    * moved attributes to method to improve overall performance

--- datagrunt/core/databases.py
+++ datagrunt/core/databases.py
@@ -13,12 +13,6 @@ class DuckDBDatabase:
     """
     DEFAULT_ENCODING = 'utf-8'
     DEFAULT_THREAD_COUNT = 16
-    JSON_OUT_FILENAME = 'output.json'
-    JSON_NEWLINE_OUT_FILENAME = 'output.jsonl'
-    CSV_OUT_FILENAME = 'output.csv'
-    EXCEL_OUT_FILENAME = 'output.xlsx'
-    PARQUET_OUT_FILENAME = 'output.parquet'
-    AVRO_OUT_FILENAME = 'output.avro'
 
     def __init__(self, filepath):
         """
@@ -33,8 +27,7 @@ class DuckDBDatabase:
         self.database_connection = self._set_database_connection()
 
     def __del__(self):
-        """Delete .db files when exiting the context manager or when connection closes."""
-        self.database_connection.close()
+        """Delete .db files after use."""
         if os.path.exists(self.database_filename):
             os.remove(self.database_filename)
 
@@ -54,26 +47,3 @@ class DuckDBDatabase:
         """
         return duckdb.connect(self.database_filename, config = {'threads': threads})
 
-    def write_to_file(self, sql_import_statement, sql_export_statement):
-        """Write database content to file.
-
-        Args:
-            sql_import_statement (str): SQL statement to import data.
-            sql_export_statement (str): SQL statement to export data.
-        """
-        con = self.database_connection
-        con.sql(sql_import_statement)
-        con.sql(sql_export_statement)
-
-    def to_dataframe(self, sql_import_statement):
-        """Export data as Polars a dataframe.
-
-        Args:
-            sql_import_statement (str): SQL statement to import data.
-
-        Return:
-            Polars dataframe.
-        """
-        con = self.database_connection
-        con.sql(sql_import_statement)
-        return con.query(self.select_from_table_statement()).pl()
