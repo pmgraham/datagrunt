7b048ee62394c35565d1bc9ee6722dc7a2813b89
Martin Graham <pmgraham@gmail.com>
2024-09-04 15:42:04 -0400

    Clarity: Refactor the application interface for clarity and modularity (#5)
    * consolidate fileproperties to filebase; reaneme module to fileproperties
    
    * rename class for clarity
    
    * move CSVParser to csvfile module for clarity
    
    * rename class for clarity
    
    * add file type check to parent class for clarity
    
    * update tests; separate method to reader and writer classes for clarity
    
    * trim whitespace
    
    * update module docstring for clarity
    
    * convert helper methods to private methods
    
    * inherit from CSVProperties to separate reader from writer classes
    
    * convert class to CSVDataframe
    
    * change back to csv reader
    
    * typo in class name
    
    * move constant to right class
    
    * instantiate reader class for dataframe
    
    * simplify reader class; add logger; add queries
    
    * update class name for clarity
    
    * update duckdb import
    
    * remove whitespaces
    
    * add blank line at bottom of file per PEP8 convention
    
    * I know this commit is too large but lots of core changes from experimentation
    
    * move class to appropriate module for clarity
    
    * move class to appropriate module for clarity
    
    * add writes for duckdb and polars engines; move outfile constants to fileproperties
    
    * line space
    
    * correct docstrings
    
    * delete blank line
    
    * delete blank line
    
    * trim whitespace
    
    * trim whitespace
    
    * convert csv attributes to class properties
    
    * update module docstring; preparing for new unit tests
    
    * add dependency for Polars write_excel method
    
    * add all unit tests for all classes in csvfile.py
    
    * remove unused import
    
    * remove unused import
    
    * reorganize constants and add is_tabular method
    
    * param spacing
    
    * update unit test params
    
    * fix attribute references
    
    * add tests for file properties classes
    
    * changed class to inherit from CSVProperties
    
    * pylint all .py files

--- datagrunt/core/logger.py
+++ datagrunt/core/logger.py
@@ -1,6 +1,10 @@
+"""Module for showing warnings."""
+
 import logging
 
-LARGE_FILE_WARNING = "Warning: File is large and may load into memory slowly or exceed memory capacity."
+LARGE_FILE_WARNING = "File is large and may load into memory slowly or exceed memory capacity."
+DUCKDB_ENGINE_ERROR = """DuckDB engine failed due to the following error: {error}. \
+    Switching to Polars."""
 
 def show_warning(message):
     """Show a warning message.
@@ -15,3 +19,9 @@ def show_warning(message):
 def show_large_file_warning():
     """Show a warning message if the file is large."""
     show_warning(LARGE_FILE_WARNING)
+
+
+def duckdb_query_error(error_message):
+    """Show error message if duckdb query fails."""
+    message = DUCKDB_ENGINE_ERROR.format(error=error_message)
+    return show_warning(message)
