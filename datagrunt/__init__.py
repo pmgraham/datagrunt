32b481073b99bf6f9da25a1077178fd07177cc47
Martin Graham <pmgraham@gmail.com>
2024-09-13 11:20:38 -0400

    update imports

--- datagrunt/__init__.py
+++ datagrunt/__init__.py
@@ -28,7 +28,7 @@ __author__ = "Martin Graham"
 __license__ = "MIT"
 
 # Import key classes, functions, or submodules that should be available at the package level
-from csvfile import CSVReader, CSVWriter
+from .csvfile import CSVReader, CSVWriter
 
 # You can define __all__ to specify what gets imported with "from package import *"
 __all__ = ['CSVReader', 'CSVWriter']
