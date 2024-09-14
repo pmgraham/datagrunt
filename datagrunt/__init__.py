d1fd3d7c30092ec20b34dcfaf7a0add567d4be4b
Martin Graham <pmgraham@gmail.com>
2024-09-14 10:08:07 -0400

    config for upload to test pypi

--- datagrunt/__init__.py
+++ datagrunt/__init__.py
@@ -23,7 +23,7 @@ Attributes:
     __license__: The license under which the package is released.
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.8"
 __author__ = "Martin Graham"
 __license__ = "MIT"
 
