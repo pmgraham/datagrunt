74db6721dd9e6987301c620993720b24157067b3
Martin Graham <pmgraham@gmail.com>
2024-09-15 05:48:50 -0400

    Publish version 0.0.0 (#12)
    * add init and contents
    
    * add pyproject toml and contents
    
    * ignore pypirc file
    
    * update imports
    
    * add name to toml
    
    * syntax fix
    
    * updated pytoml
    
    * fix imports for deployment
    
    * change version
    
    * add dependencies to toml
    
    * bump version
    
    * config for upload to test pypi
    
    * testing bumpver
    
    * bump version 0.1.9 -> 0.1.10
    
    * bump version 0.1.10 -> 0.1.11
    
    * add dev dependency
    
    * set version to 0 and name package datagrunt
    
    * test bumpver
    
    * bump version 0.0.0 -> 0.0.1
    
    * test versions
    
    * bump version 1.0.0 -> 1.0.1
    
    * move app dir to src per convention
    
    * bump version 1.0.1 -> 1.0.2
    
    * enhance toml
    
    * bump version 1.0.2 -> 1.0.3
    
    * update homepage
    
    * bump version 1.0.3 -> 1.0.4
    
    * update license reference
    
    * bump version 1.0.4 -> 1.0.5
    
    * update license reference
    
    * update readme code examples and wording
    
    * update readme wording
    
    * readme typo
    
    * reset version from testing

---
+++ src/datagrunt/__init__.py
@@ -0,0 +1,39 @@
+"""
+Datagrunt
+
+A Python library designed to simplify the way you work with CSV files.
+
+This module provides inferred CSV delimiters and helper methods for reading and writing CSV files.
+
+Example:
+    A simple example of how to use the main functionality of your package:
+
+    from datagrunt.csvfile import CSVReader
+
+    csv_file = 'electric_vehicle_population_data.csv'
+    engine = 'duckdb'
+
+    dg = CSVReader(csv_file, engine=engine)
+
+    dg.get_sample()
+
+Attributes:
+    __version__: A string representing the version of this module.
+    __author__: The name of the package author.
+    __license__: The license under which the package is released.
+"""
+
+__version__ = "0.0.0"
+__author__ = "Martin Graham"
+__license__ = "MIT"
+
+# Import key classes, functions, or submodules that should be available at the package level
+from .csvfile import CSVReader, CSVWriter
+
+# You can define __all__ to specify what gets imported with "from package import *"
+__all__ = ['CSVReader', 'CSVWriter']
+
+# Optionally, you can include a logger for your package
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
