e75d8907d1046a736fe8b911dc0089d6832926c4
Martin Graham <pmgraham@gmail.com>
2024-09-17 14:15:44 -0400

    started adding class for csv cleaning

--- src/datagrunt/csvfile.py
+++ src/datagrunt/csvfile.py
@@ -1,6 +1,7 @@
 """Module for reading CSV files and converting CSV files to different standard file formats."""
 
 # standard library
+import re
 
 # third party libraries
 import duckdb
@@ -157,9 +158,104 @@ class CSVWriter(CSVProperties):
 
 class CSVCleaner(CSVProperties):
     """Class to unify the interface for cleaning CSV files."""
+    
+    def _remove_invalid_chars(self, column_name):
+        """Leaves only alphanumeric, underscores, and whitespace characters in the string.
+        
+        Args:
+            column_name str: String to be formatted.
+        
+        Returns:
+            str: String with only alphanumeric, underscores, and whitespace characters.
+        
+        """
+        return re.sub(r'[^a-zA-Z0-9_\s]', '', column_name)
 
-    def normalize_columns():
-        pass
+    def _ensure_valid_prefix(self, column_name):
+        """Ensures that the string always starts with either a letter or an underscore
+        
+        Args:
+            column_name str: String to be formatted.
+        
+        Returns:
+            str: String that always starts with either a letter or an underscore.
+        """
+        return column_name if re.match(r'^[a-zA-Z_]', column_name) else f'_{column_name}'
+
+    def _replace_spaces(self, column_name):
+        """Converts any spaces or other whitespace characters to an underscore.
+        
+        Args:
+            column_name str: String to be formatted.
+
+        Returns:
+            str: String with spaces replaced with underscores.
+        """
+        return re.sub(r'\s+', '_', column_name)
+
+    def _remove_consecutive_duplicates(self, column_name):
+        """Removes duplicate consecutive characters from the string.
+
+        Args:
+            column_name str: String to be formatted.
+
+        Returns:
+            str: String with duplicate consecutive characters removed.
+
+        Examples:
+            - hello__world would become hello_world
+            - apple111orange would become apple1orange
+            - AABBCC would become ABC
+        """
+        return re.sub(r'([a-zA-Z0-9])\1+', r'\1', column_name)
+
+    def _normalize_column_name(self):
+        """Normalize a column name by applying cleaning methods.
+        
+        Returns:
+            column_name str: Cleaned and normalized version of the column name string.
+        """
+        column_name = column_name.lower()
+        column_name = self._remove_invalid_chars(column_name)
+        column_name = self._ensure_valid_prefix(column_name)
+        column_name = self._replace_spaces(column_name)
+        column_name = self._remove_consecutive_duplicates(column_name)
+        return column_name
+
+    def normalize_columns(self):
+        """Normalize all CSV column names and return a dict of old names to new names.
+        
+        Returns:
+            dict: old column name as the key with new column name as the value.
+        """
+        return {col: self._normalize_column_name(col) for col in self.columns}
+    
+    def update_csv_column_headers(self,
+                                  csv_output_file,
+                                  new_headers_dict,
+                                  delimiter,
+                                  line_terminator):
+        """Function does the following:
+        1. Reads existing CSV
+        2. Replaces headers with values passed in via a dictionary
+        3. Writes out the values to a new file
+        Originally the function was going to handle the file management and offer a way to
+        delete the original file and replace with the output file. However, it's best to allow
+        that to be handled downstream in order to give the most flexibility during operations.
+
+        Args:
+            csv_source_file (string): CSV file with the original headers
+            csv_output_file (string): CSV file with the updated headers
+            new_headers_dict (dict): Dictionary with the new header values
+            delimiter (string): CSV file delimiter
+            line_terminator (string): Line terminator characters
+        """
+        with open(self.filepath, 'r') as f, open(csv_output_file, 'w') as o:
+            f.readline() # and discard
+            vals = f"{delimiter}".join(new_headers_dict.values())
+            vals += line_terminator
+            o.write(vals)
+            shutil.copyfileobj(f, o)
 
     def change_encoding():
         pass
