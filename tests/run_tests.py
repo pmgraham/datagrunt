ca40561e3d38a3b0b9d3cbb9dcdf71500e666c8c
Martin Graham <pmgraham@gmail.com>
2024-09-30 08:11:23 -0400

    add statements for assessing test coverage

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,4 +4,19 @@ sys.path.append('../')  # Add the parent directory to the search path
 sys.path.append('../src/datagrunt')  # Add the parent directory to the search path
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-v", "."]))
+    # sys.exit(pytest.main(["-v", "--cov=src.datagrunt", "--cov-report=term-missing", "--cov-fail-under=90", "."]))
+    print("Python version:", sys.version)
+    print("Pytest version:", pytest.__version__)
+    
+    try:
+        import pytest_cov
+        print("Pytest-cov version:", pytest_cov.__version__)
+    except ImportError:
+        print("Pytest-cov not found!")
+
+    args = ["-v", "--cov=src.datagrunt", "--cov-report=term-missing", "."]
+    print("Running pytest with args:", args)
+    
+    exit_code = pytest.main(args)
+    print("Pytest exit code:", exit_code)
+    sys.exit(exit_code)
