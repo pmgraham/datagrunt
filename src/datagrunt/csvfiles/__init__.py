1e00f07244865a2efdeca78c980886df2ad900ef
Martin Graham <pmgraham@gmail.com>
2025-02-25 08:46:55 -0500

    merge organize branch into main
    * organize code into separate modules
    
    * remove unused import
    
    * correct docstring
    
    * remove tests and re-add later with better structure
    
    * configure framework for tests
    
    * re-enable conftest file
    
    * comment unused import
    
    * replace pandas with polars
    
    * move test files and configs to test dir
    
    * remove unused import
    
    * remove unused import
    
    * add base tests for placeholder classes
    
    * added tests and updated test config
    
    * add unit tests for more coverage
    
    * organize tests into test directories
    
    * fix typo in test dir
    
    * replace relative imports with absolute imports
    
    * consolidate imports
    
    * separate reader and writer test files
    
    * get csvreader and csvwriter to 100% test coverage
    
    * change to absolute imports in init files

---
+++ src/datagrunt/csvfiles/__init__.py
@@ -0,0 +1,6 @@
+# Import key classes, functions, or submodules that should be available at the package level
+from src.datagrunt.csvfiles.csvreader import CSVReader
+from src.datagrunt.csvfiles.csvwriter import CSVWriter
+
+# You can define __all__ to specify what gets imported with "from package import *"
+__all__ = ['CSVReader', 'CSVWriter']
