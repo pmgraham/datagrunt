14960f7e75cdf3b12ff657367a5f185180a909d0
Martin Graham <pmgraham@gmail.com>
2025-03-01 14:38:26 -0500

    removed function no longer needed

--- src/datagrunt/core/__init__.py
+++ src/datagrunt/core/__init__.py
@@ -9,8 +9,7 @@ from src.datagrunt.core.csvproperties import CSVProperties
 from src.datagrunt.core.logger import (show_warning,
                      show_info_message,
                      show_large_file_warning,
-                     duckdb_query_error,
-                     show_dataframe_sample
+                     duckdb_query_error
                     )
 from src.datagrunt.core.queries import DuckDBQueries
 
