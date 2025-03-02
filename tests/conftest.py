9c1f205b00491c05b0876fddbce6535f61d731ae
Martin Graham <pmgraham@gmail.com>
2025-03-02 07:53:47 -0500

    changed class name for clarity of its purpose

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append('../')  # Add the parent directory to the search path
 sys.path.append('../src/datagrunt')  # Add the parent directory to the search path
 
-from src.datagrunt.core.csvproperties import CSVFormatter
+from src.datagrunt.core.csvproperties import CSVColumnFormatter
 
 
 # Dummy CSV data for testing
@@ -65,7 +65,7 @@ def create_large_csv(tmp_path):
 
 @pytest.fixture
 def formatter(sample_csv_path):
-    return CSVFormatter(sample_csv_path)  # Path doesn't matter for these tests
+    return CSVColumnFormatter(sample_csv_path)  # Path doesn't matter for these tests
 
 @pytest.fixture
 def empty_csv_path(tmp_path):
