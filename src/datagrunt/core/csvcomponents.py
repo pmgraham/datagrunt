71468a41b29a1972df7fd0945516e036e43b5aee
Martin Graham <pmgraham@gmail.com>
2025-03-03 08:02:26 -0500

    move properties to better align with classes

--- src/datagrunt/core/csvcomponents.py
+++ src/datagrunt/core/csvcomponents.py
@@ -55,79 +55,6 @@ class CSVDelimiter:
             delimiter = delimiter_candidates[0][0]
         return delimiter
 
-class CSVColumnNameNormalizer:
-    """Class to normalize CSV columns names."""
-
-    SPECIAL_CHARS_PATTERN = re.compile(r'[^a-z0-9]+')
-    MULTI_UNDERSCORE_PATTERN = re.compile(r'_+')
-
-    def __init__(self, filename):
-        """Initialize the CSVColumnNameNormalizer with a filename."""
-        self.filename = filename
-        self.columns_normalized = self.normalize_column_names(self._columns())
-
-    def _columns(self):
-        """Return the list of columns from the CSV file."""
-        return CSVColumns(self.filename).columns
-
-    def _normalize_single_column_name(self, column_name):
-        """Normalize a single column name by converting to lowercase, replacing spaces and special
-        characters with underscores, and removing extra underscores.
-
-        Replace special characters and spaces with underscore
-        Remove leading and trailing underscores
-        Replace multiple underscores with single underscore
-        Add a leading underscore if the name starts with a digit
-
-        Args:
-            column_name (str): The column name to normalize
-
-        Returns:
-            str: The normalized column name
-        """
-        name = column_name.lower()
-        name = self.SPECIAL_CHARS_PATTERN.sub('_', name)
-        name = name.strip('_')
-        name = self.MULTI_UNDERSCORE_PATTERN.sub('_', name)
-        return f'_{name}' if name and name[0].isdigit() else name
-
-    def _make_unique_column_names(self, columns_list):
-        """
-        Make unique column names by appending a number to duplicate names.
-
-        Args:
-            columns (list): List of column names to make unique
-
-        Returns:
-            list: List of unique column names
-        """
-        name_count = {}
-        unique_names = []
-
-        for name in columns_list:
-            if name in name_count:
-                name_count[name] += 1
-                unique_names.append(f"{name}_{name_count[name]}")
-            else:
-                name_count[name] = 0
-                unique_names.append(name)
-
-        return unique_names
-
-    def normalize_column_names(self, columns):
-        """
-        Normalize column names by converting to lowercase, replacing spaces and special
-        characters with underscores, and removing extra underscores.
-
-        Args:
-            columns (list): List of column names to normalize
-
-        Returns:
-            list: List of normalized column names
-        """
-        normalized_columns = [self._normalize_single_column_name(col) for col in columns]
-        return self._make_unique_column_names(normalized_columns)
-
 class CSVDialect:
     """Class for inferring the CSV dialect."""
 
@@ -218,7 +145,6 @@ class CSVColumns:
         """Initialize the CSVColumns class."""
         self.filepath = filepath
         self.columns = CSVRows(filepath).first_row.split(CSVDelimiter(filepath).delimiter)
-        self.columns_normalized = CSVColumnNameNormalizer(filepath).columns_normalized
 
     @property
     def columns_string(self):
@@ -226,14 +152,88 @@ class CSVColumns:
         return ', '.join(self.columns)
 
     @property
-    def columns_normalized_string(self):
-        """Return a list representation of the normalized columns."""
-        return ', '.join(self.columns_normalized)
-
-    @property
     def columns_byte_string(self):
         """Return a byte string representation of the columns."""
         return ', '.join(self.columns).encode()
+
+    @property
+    def columns_count(self):
+        """Return the number of columns."""
+        return len(self.columns)
+
+class CSVColumnNameNormalizer:
+    """Class to normalize CSV columns names."""
+
+    SPECIAL_CHARS_PATTERN = re.compile(r'[^a-z0-9]+')
+    MULTI_UNDERSCORE_PATTERN = re.compile(r'_+')
+
+    def __init__(self, filename):
+        """Initialize the CSVColumnNameNormalizer with a filename."""
+        self.filename = filename
+        self.columns_normalized = self._normalize_column_names(CSVColumns(filename).columns)
+
+    def _normalize_single_column_name(self, column_name):
+        """Normalize a single column name by converting to lowercase, replacing spaces and special
+        characters with underscores, and removing extra underscores.
+
+        Replace special characters and spaces with underscore
+        Remove leading and trailing underscores
+        Replace multiple underscores with single underscore
+        Add a leading underscore if the name starts with a digit
+
+        Args:
+            column_name (str): The column name to normalize
+
+        Returns:
+            str: The normalized column name
+        """
+        name = column_name.lower()
+        name = self.SPECIAL_CHARS_PATTERN.sub('_', name)
+        name = name.strip('_')
+        name = self.MULTI_UNDERSCORE_PATTERN.sub('_', name)
+        return f'_{name}' if name and name[0].isdigit() else name
+
+    def _make_unique_column_names(self, columns_list):
+        """
+        Make unique column names by appending a number to duplicate names.
+
+        Args:
+            columns (list): List of column names to make unique
+
+        Returns:
+            list: List of unique column names
+        """
+        name_count = {}
+        unique_names = []
+
+        for name in columns_list:
+            if name in name_count:
+                name_count[name] += 1
+                unique_names.append(f"{name}_{name_count[name]}")
+            else:
+                name_count[name] = 0
+                unique_names.append(name)
+
+        return unique_names
+
+    def _normalize_column_names(self, columns):
+        """
+        Normalize column names by converting to lowercase, replacing spaces and special
+        characters with underscores, and removing extra underscores.
+
+        Args:
+            columns (list): List of column names to normalize
+
+        Returns:
+            list: List of normalized column names
+        """
+        normalized_columns = [self._normalize_single_column_name(col) for col in columns]
+        return self._make_unique_column_names(normalized_columns)
+
+    @property
+    def columns_normalized_string(self):
+        """Return a list representation of the normalized columns."""
+        return ', '.join(self.columns_normalized)
 
     @property
     def columns_normalized_byte_string(self):
@@ -243,9 +243,4 @@ class CSVColumns:
     @property
     def columns_to_normalized_mapping(self):
         """Return the mapping of original column names to normalized column names."""
-        return dict(OrderedDict(zip(self.columns, self.columns_normalized)))
-
-    @property
-    def columns_count(self):
-        """Return the number of columns."""
-        return len(self.columns)
+        return dict(OrderedDict(zip(CSVColumns(self.filename).columns, self.columns_normalized)))
