74722b4214cd21ffbb12dc4db5117b0c6fef53b8
Martin Graham <pmgraham@gmail.com>
2025-03-08 08:42:35 -0500

    fix all imports

--- src/datagrunt/csvfiles/csvwriter.py
+++ src/datagrunt/csvfiles/csvwriter.py
@@ -5,9 +5,9 @@
 # third party libraries
 
 # local libraries
-from src.datagrunt.core import CSVComponents
-from src.datagrunt.core import DuckDBQueries
-from src.datagrunt.core import CSVEngineFactory
+from datagrunt.core import CSVComponents
+from datagrunt.core import DuckDBQueries
+from datagrunt.core import CSVEngineFactory
 
 class CSVWriter(CSVComponents):
     """Class to unify the interface for converting CSV files to various other supported file types."""
