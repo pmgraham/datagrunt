96fd2580c1b57dd8ed3625e54d9c6000c979b40f
Martin Graham <pmgraham@gmail.com>
2025-06-28 08:14:54 -0400

    bump version 1.0.10 -> 1.0.11

--- src/datagrunt/__init__.py
+++ src/datagrunt/__init__.py
@@ -23,7 +23,7 @@ Attributes:
     __license__: The license under which the package is released.
 """
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 __author__ = "Martin Graham"
 __license__ = "MIT"
 
