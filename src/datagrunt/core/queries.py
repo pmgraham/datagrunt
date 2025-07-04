31cf3e2ed5b6a4cfd25bb30b18b7d4252d282e8f
Martin Graham <pmgraham@gmail.com>
2025-07-04 12:55:13 -0400

    further refactor the normalize columns method for duckdb

--- src/datagrunt/core/queries.py
+++ src/datagrunt/core/queries.py
@@ -154,7 +154,8 @@ class DuckDBQueries:
         consistent naming conventions across different processing engines.
         """
         duckdb.sql(self.import_csv_query())
-        for old_name, new_name in zip(CSVColumns(self.filepath).columns,
+        table_columns = duckdb.sql(f"SELECT * FROM {self.database_table_name} LIMIT 0").columns
+        for old_name, new_name in zip(table_columns,
                                       CSVColumnNameNormalizer(self.filepath).columns_normalized
                                       ):
             sql_string = f"ALTER TABLE {self.database_table_name} RENAME COLUMN \"{old_name}\" TO \"{new_name}\""
