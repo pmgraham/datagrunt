858fcbf68f6b7b214ccb76944f382e5b738624fb
Martin Graham <pmgraham@gmail.com>
2025-07-11 05:42:28 -0400

    added class for creating ai factory

---
+++ src/datagrunt/ai/factories.py
@@ -0,0 +1,35 @@
+"""Factor module for creating AI factory instances."""
+
+# local libraries
+
+from datagrunt.ai.engines import EngineProperties
+from datagrunt.ai.engines import GoogleEngine
+
+class AIEngineFactory:
+    """Factory class for creating AI engine instances."""
+    AI_ENGINES = {
+                'google': GoogleEngine,
+            }
+
+    def __init__(self, api_key, engine, **kwargs):
+        """
+        Initialize the AI Engine Factory class.
+
+        Args:
+            api_key (str): API key for the AI provider.
+            engine (str): type of engine to create by the factory.
+            **kwargs: Additional parameters for the AI provider.
+        """
+        self.api_key = api_key
+        self.engine = engine.lower().replace(' ', '')
+        self.kwargs = kwargs
+        if self.engine not in EngineProperties.valid_engines:
+            raise ValueError(EngineProperties.value_error_message.format(engine=self.engine))
+
+    def create_engine(self):
+        """Create an AI engine instance."""
+        engine_class = self.AI_ENGINES.get(self.engine)
+        if engine_class:
+            return engine_class(self.api_key, **self.kwargs)
+        else:
+            raise ValueError(f"Unsupported AI engine: {self.engine}")
