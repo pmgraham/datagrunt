ddbca08d06ee9fb3b4c4b899d761d3a7d724deae
Martin Graham <pmgraham@gmail.com>
2025-07-11 06:06:01 -0400

    rename class for clarity and expose in init files

--- src/datagrunt/csvfiles/__init__.py
+++ src/datagrunt/csvfiles/__init__.py
@@ -1,8 +1,8 @@
 # Import key classes, functions, or submodules that should be available at the package level
 from datagrunt.csvfiles.csvreader import CSVReader
 from datagrunt.csvfiles.csvwriter import CSVWriter
-from datagrunt.csvfiles.csvai import CSVSchemaReport
+from datagrunt.csvfiles.csvai import CSVSchemaReportAIGenerated
 
 
 # You can define __all__ to specify what gets imported with "from package import *"
-__all__ = ['CSVReader', 'CSVWriter', 'CSVSchemaReport']
+__all__ = ['CSVReader', 'CSVWriter', 'CSVSchemaReportAIGenerated']
