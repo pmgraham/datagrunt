8ecdd4f84916d630775562fe2d676b7d548f7de0
Martin Graham <pmgraham@gmail.com>
2025-07-11 06:08:56 -0400

    rename class for clarity

--- src/datagrunt/ai/engines.py
+++ src/datagrunt/ai/engines.py
@@ -71,7 +71,7 @@ class BaseAIEngine(ABC):
         """
         pass
 
-class GoogleEngine(BaseAIEngine):
+class GoogleAIEngine(BaseAIEngine):
     """Class to interact with the Google GenAI API."""
 
     THINKING_BUDGET = -1  # Default thinking budget for the model
