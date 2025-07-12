188806b164d00f98ce39a5a0de2ae6f581f891d5
Martin Graham <datagrunt@datagrunt.io>
2025-07-12 00:21:37 -0400

    rename csvfiles module for clarity

--- src/datagrunt/__init__.py
+++ src/datagrunt/__init__.py
@@ -28,7 +28,7 @@ __author__ = "Martin Graham"
 __license__ = "MIT"
 
 # Import key classes, functions, or submodules that should be available at the package level
-from datagrunt.csvfiles import CSVReader, CSVWriter, CSVSchemaReportAIGenerated
+from datagrunt.csv_api import CSVReader, CSVWriter, CSVSchemaReportAIGenerated
 
 
 # You can define __all__ to specify what gets imported with "from package import *"
