63bea1512e138d7065c18e34f1c6f5e410fe627e
Martin Graham <pmgraham@gmail.com>
2026-06-12 16:51:14 -0400

    feat(rust): io helpers - utf-8-sig ignore decode, universal newlines, file probes

--- rust/datagrunt-python/src/lib.rs
+++ rust/datagrunt-python/src/lib.rs
@@ -1,12 +1,6 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn smoke() -> &'static str {
-    datagrunt_core::smoke()
-}
-
 #[pymodule]
-fn datagrunt_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(smoke, m)?)?;
+fn datagrunt_rs(_m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
